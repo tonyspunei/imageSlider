9be8bab9a63a8b67fffbde5ce9f636e8de882d90
tonyspunei <tony.spunei@yahoo.com>
2020-12-01 12:46:38 +0100

    done with imageslider project

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 (function () {
   let backgrondImg = document.getElementById("image");
   let buttons = document.querySelectorAll(".btn");
-  let buttonLeft = document.getElementById("btn-left");
-  let buttonRight = document.getElementById("btn-right");
-  let images = ["img1.jpg", "img2.jpg", "img3.jpg"];
+  let images = [
+    "img1.jpg",
+    "img2.jpg",
+    "img3.jpg",
+    "img4.jpg",
+    "img5.jpg",
+    "img6.jpg",
+    "img7.jpg",
+    "img8.jpg",
+  ];
+  let index = 0;
 
-  buttonRight.addEventListener("click", swipe());
+  backgrondImg.style.backgroundImage = `url(/img/${images[index]})`;
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      if (button.classList.contains("btn-left")) {
+        index--;
+      }
+      backgrondImg.style.backgroundImage = `url(./img/${images[index]})`;
+
+      if (button.classList.contains("btn-right")) {
+        index++;
+        if (index > images.length - 1 || index < 0) {
+          index = 0;
+        }
+        backgrondImg.style.backgroundImage = `url(./img/${images[index]})`;
+      }
+    });
+  });
 })();
